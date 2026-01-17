e2fc6b78c9b9b2e6a25c4f05351a4f64379607fd
b3rn1eBucks <davonne.campbell@gmail.com>
2026-01-17 15:10:53 -0400

    Update sheets-to-posts.php

--- sheets-to-posts.php
+++ sheets-to-posts.php
@@ -5,3 +5,31 @@
  * Version: 0.1.0
  * Author: Your Name
  */
+
+if (!defined('ABSPATH')) {
+  exit;
+}
+
+add_action('admin_menu', 's2p_add_admin_menu');
+
+function s2p_add_admin_menu() {
+  add_options_page(
+    'Sheets to Posts',
+    'Sheets to Posts',
+    'manage_options',
+    'sheets-to-posts',
+    's2p_render_settings_page'
+  );
+}
+
+function s2p_render_settings_page() {
+  if (!current_user_can('manage_options')) {
+    return;
+  }
+  ?>
+  <div class="wrap">
+    <h1>Sheets to Posts</h1>
+    <p>Your plugin is installed and working. Next we’ll add your Google Sheet connection here.</p>
+  </div>
+  <?php
+}
